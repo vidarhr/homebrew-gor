12775150db6abfd7bb402bebe402b1ac46685777
Viðar Hrafnkelsson <vidar@wuxinextcode.com>
2020-06-30 12:45:44 +0000

    Added sha

--- gorpipe.rb
+++ gorpipe.rb
@@ -3,6 +3,7 @@ class Gorpipe < Formula
   homepage "https://github.com/gorpipe/gor"
   version = "2.8.0"
   url "https://github.com/gorpipe/gor/releases/download/v" + version + "/gor-scripts-" + version + ".zip"
+  sha256 "808171d4d106fe5de500476cbe6371bf1dfa78db2317e29de194019f3f9291b7"
 
   def install
     prefix.install Dir["*"]
